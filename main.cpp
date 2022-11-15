bd0533bdd82d56742b277522a3af641b1a7c0d0f
SimoneCortese <117988407+SimoneCortese@users.noreply.github.com>
2022-11-15 22:14:37 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,17 +1,15 @@
 #include <iostream>
 using namespace std;
-
 int main() {
-    int a, b, x, y;
-    cout<<"inserire i valori di a, b, x, y";
+    int a,b,x,y;
+    cout<<"inserire i valori di a , b, x , y"<<endl;
     cin>>a>>b>>x>>y;
-    if (x<0 && y>0){
-        cout<<"z ="<<(a*x)-(b*y);
+    if (x<0 && y>0) {
+        cout<<"z = "<<(a*x)-(b*y);
     }
-    else if (x>=0 && y<=0){
-    cout<<"z ="<<(a*(x*x))-(b*y);
-        }
-    else cout<<"z ="<<(a*x)+(b*(y*y));
-    
-    return 0;
+else if (x>=0 && y<=0){
+        cout<<"z = "<<(a*(x*x))-(b*y);
+}
+else cout<<"z= "<<(a*x)+(y*y*b);
+return 0;
 }
